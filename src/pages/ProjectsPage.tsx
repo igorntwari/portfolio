9b4b8db6299c387f08554c7885ea29531dd7feb8
igorntwari <igorntwari28@gmail.com>
2023-12-24 14:00:26 +0200

    fix:improved code base

--- src/pages/ProjectsPage.tsx
+++ src/pages/ProjectsPage.tsx
@@ -115,23 +115,6 @@ const ProjectsPages = () => {
             </div>
           </div>
         </div>
-        <div className="bg-zinc-800 rounded-xl flex flex-col gap-2">
-          <img className="object-contain" src={image_33} alt="projectImage" />
-          <div className="flex justify-between px-4 items-center">
-            <div className="flex flex-col gap-2">
-              <h1 className="font-Manrope-Regular text-green-700 text-sm sm:text-xl">
-                App Design
-              </h1>
-              <h1 className="font-Manrope-Regular sm:text-xl text-xs">
-                Team Wave In React Router
-              </h1>
-              <div className="flex gap-2 justify-center py-2">
-                <button className="bg-black rounded-md px-8">Source Code</button>
-                <button className="bg-black rounded-md px-8">Live Preview</button>
-              </div>
-            </div>
-          </div>
-        </div>
       </div>
     </div>
   );
