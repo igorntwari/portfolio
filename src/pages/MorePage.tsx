2e3ca844b18d7a9f20ff15899cbbb2f5baa7e2b9
igorntwari <igorntwari28@gmail.com>
2024-01-03 16:36:46 +0200

    feat: added changes

--- src/pages/MorePage.tsx
+++ src/pages/MorePage.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 function MorePage() {
   return (
-    <div className="bg-black">
+    <div className="bg-black text-white">
      <h1>this is a review</h1>
     </div>
   );
