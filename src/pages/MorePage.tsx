9e60817fb305bf2a2ac857bf999112c972b617ed
igorntwari <igorntwari28@gmail.com>
2024-02-02 20:57:36 +0200

    fix:made some changes

--- src/pages/MorePage.tsx
+++ src/pages/MorePage.tsx
@@ -3,7 +3,7 @@ function MorePage() {
   return (
     <div className="bg-black text-white">
      <h1 className="font-Manrope-light text-center text-4xl uppercase h-screen">review page</h1>
-    <h1 className="">please a review for me</h1>
+    <h1 className="font-Manrope-Regular">please a review for me</h1>
     </div>
   );
 }
