39996e62e1b2886ecb1aad05588f824e941ce9e5
igorntwari <igorntwari28@gmail.com>
2024-02-03 20:58:01 +0200

    fix:made some changes

--- src/pages/MorePage.tsx
+++ src/pages/MorePage.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 function MorePage() {
   return (
-    <div className="bg-black text-white">
+    <div className=" text-white">
      <h1 className="font-Manrope-light text-center text-4xl uppercase h-screen">review page</h1>
     <h1 className="font-Manrope-Regular">please a review for me</h1>
     </div>
